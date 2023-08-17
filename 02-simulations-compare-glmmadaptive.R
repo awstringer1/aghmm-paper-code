7cf2cdd8a323438c94e4a9c2b0536224deb6de64
awstringer1 <awstringer@gmail.com>
2023-08-17 11:55:03 -0400

    sims 2

--- 02-simulations-compare-glmmadaptive.R
+++ 02-simulations-compare-glmmadaptive.R
@@ -1,5 +1,5 @@
-### Simulations to replicate Section 4.3, simulation 1
-### in the paper.
+### Simulations to replicate Section 4.3, simulation 2,
+### comparison to GLMMadaptive, in the paper.
 ### Alex Stringer
 ### 2023/08
 
@@ -7,7 +7,7 @@
 # CHANGE the base path to whatever you want on your machine
 basepath <- '~/work/projects/mixedmodel-computation/replication'
 # CHANGE the name of the simulation to control how saved results are named
-simname <- "sims-20230817-v1"
+simname <- "sims-glmma-20230817-v1"
 stopifnot(dir.exists(basepath))
 resultspath <- file.path(basepath,'results')
 if (!dir.exists(resultspath)) dir.create(resultspath)
@@ -40,13 +40,14 @@ for (pkg in pkgs) {
   }
 }
 
+
 ## Set Parameters ##
 # Will run numsims simulations in parallel, repeated numruns times in series.
-numruns <- 10  # Number of times to execute the simulations
-numsims <- 100 # Number of simulations in each category PER RUN
-m <- c(100,200,500,1000)
+numruns <- 1  # Number of times to execute the simulations
+numsims <- 500 # Number of simulations in each category PER RUN
+m <- c(100,200,500,1000) # m,n chosen from main manuscript
 n <- c(3,5,7,9)
-k <- c(1,3,5,7,9,11,13,15,17,19,21,23,25)
+k <- seq(1,25,by=2)
 beta <- c(-2.5,-.15,.1,.2)
 S <- matrix(c(2,1,1,1),ncol=2)
 Q <- solve(S)
@@ -109,6 +110,7 @@ RNGkind("L'Ecuyer-CMRG") # For reproducibility with parallel
 
 dosim <- function(lst) {
   # lst: simulation parameters
+  idx <-          lst$idx           # Keep track of which simulation
   m <-            lst$m             # Number of groups
   n <-            lst$n             # Number of observations per group
   beta <-         lst$beta          # Regression parameters
@@ -127,80 +129,126 @@ dosim <- function(lst) {
   tol <-            1e-05 # Tolerance for gradient norm of outer optimization
   max_linesearch <- 100   # Maximum number of iterations for the line search at each step 
   
-  cat("Sim: ",lst$idx," of ",length(simlist),"|m=",m,"|n=",n,"|k=",k,"...",sep="")
+  cat("Sim: ",lst$idx," of ",length(simlist),"|m=",m,"|n=",n,"|k=",k,"... ",sep="")
   
   # Create control list
   bfgscontrol <- aghqmm::aghqmm_control()
   
   # Simulate data
   simdata <- aghqmm::simulate_data(m,n,beta,S)
-  # Fit model
-  opt <- tryCatch(aghqmm::aghqmm(y ~ x*t + (t|id),simdata,k=k,method = "both",control = bfgscontrol),error = function(e) e)
+  # Fit model using both methods
+  cat("doing aghq... ")
+  opt_aghqmm <- tryCatch(aghqmm::aghqmm(y ~ x*t + (t|id),simdata,k=k,method = "both",control = bfgscontrol),
+                         warning = function(w) w,
+                         error = function(e) e)
   
-  if (inherits(opt,'condition')) return(opt)
+  cat("doing glmmadaptive... ")
+  opt_glmmadaptive <- tryCatch(aghqmm::aghqmm(y ~ x*t + (t|id),simdata,k=k,method = "GLMMadaptive",control = bfgscontrol),
+                               warning = function(w) w,
+                               error = function(e) e)
   
-  # Return results
-  simresults <- data.frame(
-    m=m,n=n,k=k,inner_tol=inner_tol,inner_maxitr=inner_maxitr,bfgshist=bfgshist,past=past,
-    totaltime = opt$comptime
-  )
-  # get the true sigmas from delta/phi
   truesigmas <- c(diag(S),S[2,1])
-  estsigmas <- opt$sigmaints[ ,2]
+  totalsamplesize <- nrow(simdata)
   
-  paramresults <- data.frame(
-    true = c(beta,truesigmas),
-    est = c(opt$theta[1:4],estsigmas),
-    lowerWald = c(opt$betaints[ ,1],opt$sigmaints[ ,1]),
-    upperWald = c(opt$betaints[ ,3],opt$sigmaints[ ,3])
-  ) %>%
-    dplyr::mutate(
-      covrWald = lowerWald <= true & upperWald >= true,
-      lengthWald = upperWald - lowerWald
+  if (inherits(opt_aghqmm,'condition')) {
+    results_aghqmm <- opt_aghqmm
+  } else {
+    estsigmas_aghqmm <- opt_aghqmm$sigmaints[ ,2]
+    simresults_aghqmm <- data.frame(
+      totaltime = opt_aghqmm$comptime,
+      nll = opt_aghqmm$nll,
+      nllavg = opt_aghqmm$nll/totalsamplesize,
+      nllavgbase10 = (opt_aghqmm$nll/log(10))/totalsamplesize,
+      normgrad = opt_aghqmm$normgrad_2,
+      normgradlogbase10avg = log(opt_aghqmm$normgrad_2,base=10) - log(totalsamplesize,base=10)
     )
-  cat(" completed.\n",sep="")
-  list(simresults=simresults,paramresults=paramresults)
+    paramresults_aghqmm <- data.frame(
+      true = c(beta,truesigmas),
+      est = c(opt_aghqmm$theta[1:4],estsigmas_aghqmm),
+      lowerWald = c(opt_aghqmm$betaints[ ,1],opt_aghqmm$sigmaints[ ,1]),
+      upperWald = c(opt_aghqmm$betaints[ ,3],opt_aghqmm$sigmaints[ ,3])
+    ) %>%
+      dplyr::mutate(
+        covrWald = lowerWald <= true & upperWald >= true,
+        lengthWald = upperWald - lowerWald
+      )
+    results_aghqmm <- list(simresults=simresults_aghqmm,paramresults=paramresults_aghqmm)
+  }
+  
+  if (inherits(opt_glmmadaptive,'condition')) {
+    results_glmmadaptive <- opt_glmmadaptive
+  } else {
+    estsigmas_glmmadaptive <- opt_glmmadaptive$sigmaints[ ,2]
+    simresults_glmmadaptive <- data.frame(
+      totaltime = opt_glmmadaptive$comptime,
+      nll = opt_glmmadaptive$nll,
+      nllavg = opt_glmmadaptive$nll/totalsamplesize,
+      nllavgbase10 = (opt_glmmadaptive$nll/log(10))/totalsamplesize,
+      normgrad = opt_glmmadaptive$normgrad_2,
+      normgradlogbase10avg = log(opt_glmmadaptive$normgrad_2,base=10) - log(totalsamplesize,base=10)
+    )
+    paramresults_glmmadaptive <- data.frame(
+      true = c(beta,truesigmas),
+      est = c(opt_glmmadaptive$theta[1:4],estsigmas_glmmadaptive),
+      lowerWald = c(opt_glmmadaptive$betaints[ ,1],opt_glmmadaptive$sigmaints[ ,1]),
+      upperWald = c(opt_glmmadaptive$betaints[ ,3],opt_glmmadaptive$sigmaints[ ,3])
+    ) %>%
+      dplyr::mutate(
+        covrWald = lowerWald <= true & upperWald >= true,
+        lengthWald = upperWald - lowerWald
+      )
+    results_glmmadaptive <- list(simresults=simresults_glmmadaptive,paramresults=paramresults_glmmadaptive)
+  }
+  cat("completed.\n",sep="")
+  list(paramvalues = list(idx=idx,m=m,n=n,k=k,inner_tol=inner_tol,inner_maxitr=inner_maxitr,bfgshist=bfgshist,past=past),
+       aghqmm = results_aghqmm,
+       glmmadaptive = results_glmmadaptive,
+       data = simdata,
+       opt = list(aghqmm=opt_aghqmm,glmmadaptive=opt_glmmadaptive))
 }
 
 processsimulation <- function(sim) {
-  if (inherits(sim,'condition')) return(NULL)
-  # Change this if the parameters change
-  out <- sim$simresults
-  out$beta1true <- sim$paramresults[1,'true']
-  out$beta1est <- sim$paramresults[1,'est']
-  out$beta1bias <- out$beta1est-out$beta1true
-  out$beta1covrWald <- sim$paramresults[1,'covrWald']
-  out$beta1lengthWald <- sim$paramresults[1,'lengthWald']
-  out$beta2true <- sim$paramresults[2,'true']
-  out$beta2est <- sim$paramresults[2,'est']
-  out$beta2bias <- out$beta2est-out$beta2true
-  out$beta2covrWald <- sim$paramresults[2,'covrWald']
-  out$beta2lengthWald <- sim$paramresults[2,'lengthWald']
-  out$beta3true <- sim$paramresults[3,'true']
-  out$beta3est <- sim$paramresults[3,'est']
-  out$beta3bias <- out$beta3est-out$beta3true
-  out$beta3covrWald <- sim$paramresults[3,'covrWald']
-  out$beta3lengthWald <- sim$paramresults[3,'lengthWald']
-  out$beta4true <- sim$paramresults[4,'true']
-  out$beta4est <- sim$paramresults[4,'est']
-  out$beta4bias <- out$beta4est-out$beta4true
-  out$beta4covrWald <- sim$paramresults[4,'covrWald']
-  out$beta4lengthWald <- sim$paramresults[4,'lengthWald']
-  out$sigmasq1true <- sim$paramresults[5,'true']
-  out$sigmasq1est <- sim$paramresults[5,'est']
-  out$sigmasq1bias <- out$sigmasq1est-out$sigmasq1true
-  out$sigmasq1covrWald <- sim$paramresults[5,'covrWald']
-  out$sigmasq1lengthWald <- sim$paramresults[5,'lengthWald']
-  out$sigmasq2true <- sim$paramresults[6,'true']
-  out$sigmasq2est <- sim$paramresults[6,'est']
-  out$sigmasq2bias <- out$sigmasq2est-out$sigmasq2true
-  out$sigmasq2covrWald <- sim$paramresults[6,'covrWald']
-  out$sigmasq2lengthWald <- sim$paramresults[6,'lengthWald']
-  out$sigmacov1true <- sim$paramresults[7,'true']
-  out$sigmacov1est <- sim$paramresults[7,'est']
-  out$sigmacov1bias <- out$sigmacov1est-out$sigmacov1true
-  out$sigmacov1covrWald <- sim$paramresults[7,'covrWald']
-  out$sigmacov1lengthWald <- sim$paramresults[7,'lengthWald']
+  
+  out_aghqmm <- with(sim$paramvalues,data.frame(
+    idx = idx,
+    method = "aghqmm",
+    m = m,
+    n = n,
+    k = k
+  ))
+  out_glmmadaptive <- with(sim$paramvalues,data.frame(
+    idx = idx,
+    method = "glmmadaptive",
+    m = m,
+    n = n,
+    k = k
+  ))
+  
+  if (inherits(sim$aghqmm,"condition")) {
+    out_aghqmm['successful'] <- 0
+    out_aghqmm['comptime'] <- 0
+    out_aghqmm['nllavgbase10'] <- 0
+    out_aghqmm['normgradavg2base10log'] <- 0
+  } else {
+    out_aghqmm['successful'] <- 1
+    out_aghqmm['comptime'] <- sim$aghqmm$simresults$totaltime
+    out_aghqmm['nllavgbase10'] <- sim$aghqmm$simresults$nllavgbase10
+    out_aghqmm['normgradavg2base10log'] <- sim$aghqmm$simresults$normgradlogbase10avg
+  }
+  if (inherits(sim$glmmadaptive,"condition")) {
+    out_glmmadaptive['successful'] <- 0
+    out_glmmadaptive['comptime'] <- 0
+    out_glmmadaptive['nllavgbase10'] <- 0
+    out_glmmadaptive['normgradavg2base10log'] <- 0
+  } else {
+    out_glmmadaptive['successful'] <- 1
+    out_glmmadaptive['comptime'] <- sim$glmmadaptive$simresults$totaltime
+    out_glmmadaptive['nllavgbase10'] <- sim$glmmadaptive$simresults$nllavgbase10
+    out_glmmadaptive['normgradavg2base10log'] <- sim$glmmadaptive$simresults$normgradlogbase10avg
+  }
+  
+  out <- rbind(out_aghqmm,out_glmmadaptive)
+  
   out
 }
 
@@ -221,11 +269,13 @@ simtime <- as.numeric(difftime(Sys.time(),tm,units='secs'))
 cat("Finished simulations, they took",simtime,"seconds.\n")
 cat("Saving simulations...\n")
 save(sims,file=file.path(resultspath,simresultsname))
+#load(file=file.path(resultspath,simresultsname))
 cat("Saved simulations to file:",file.path(resultspath,simresultsname),"\n")
 cat("Processing simulations...\n")
 simsprocessed <- dplyr::bind_rows(lapply(sims,processsimulation))
 readr::write_csv(simsprocessed,file=file.path(resultspath,simsprocessedname))
 cat("Finished processing simulations.\n")
 cat("Wrote results to:",file.path(resultspath,simsprocessedname),"\n")
-cat("Next, execute the script '01-summarize-simulations-absolute.R'\n")
+cat("Next, execute the script '02-summarize-simulations-compare-glmmadaptive.R'\n")
+
 
