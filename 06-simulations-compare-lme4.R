aa27a9b8afcfb1bf2fe79315362d42871282db26
awstringer1 <awstringer@gmail.com>
2024-08-02 15:05:41 -0400

    trying 06 after fixing error

--- 06-simulations-compare-lme4.R
+++ 06-simulations-compare-lme4.R
@@ -4,18 +4,16 @@
 
 ## Set paths ##
 # CHANGE the base path to whatever you want on your machine
-basepath <- '~/work/projects/mixedmodel-computation/replication'
+basepath <- getwd()
 # CHANGE the name of the simulation to control how saved results are named
-simname <- "sims-lme4-20230817-v1"
+simname <- "sims-lme4-20240802-v1"
 stopifnot(dir.exists(basepath))
 resultspath <- file.path(basepath,'results')
 if (!dir.exists(resultspath)) dir.create(resultspath)
+figurepath <- file.path(basepath,'figures')
+if (!dir.exists(figurepath)) dir.create(figurepath)
 simresultsname <- paste0(simname,".RData")
 simsprocessedname <- paste0(simname,".csv")
-
-# CHANGE the path to where you downloaded the aghqmm package repo
-# from https://github.com/awstringer1/aghqmm
-aghqmmpath <- "~/work/projects/mixedmodel-computation/aghqmm" # CHANGE this
 
 # Everything else in the script should run without changes
 
@@ -24,11 +22,16 @@ aghqmmpath <- "~/work/projects/mixedmodel-computation/aghqmm" # CHANGE this
 install.packages(aghqmmpath,repos=NULL,type="source")
 
 pkgs <- c(
-  'tidyverse',
+  'ggplot2',
+  'dplyr',
+  'tidyr',
+  'readr',
+  'fastmatrix',
   'lme4',
   'Rcpp',
   'RcppEigen',
-  'parallel'
+  'remotes'
+  # 'parallel'
 )
 for (pkg in pkgs) {
   if (!require(pkg,character.only = TRUE,quietly = TRUE)) {
@@ -38,13 +41,33 @@ for (pkg in pkgs) {
   }
 }
 
+## Install aghqmm
+
+# Local
+# If installing remotely from Github doesn't work, download the package repository to your basepath
+# and uncomment the next two lines:
+# aghqmmpath <- file.path(basepath, "aghqmm")
+# install.packages(aghqmmpath, repos=NULL, type="source")
+# Remote: if you have a Github PAT set up in your R session, this should work:
+remotes::install_github("awstringer1/aghqmm", force = TRUE)
+# If you want to set up remotes, this tutorial is helpful:
+# https://carpentries.github.io/sandpaper-docs/github-pat.html
+library(aghqmm)
 
 ## Set Parameters ##
-numruns <- 2  # Number of times to execute the simulations
-numsims <- 10 # Number of simulations in each category PER RUN
-m <- c(100,200,500,1000) # m,n chosen from main manuscript
-n <- c(3,5,7,9)
-k <- seq(1,25,by=2)
+# These settings were used for the paper:
+# numruns <- 2  # Number of times to execute the simulations
+# numsims <- 10 # Number of simulations in each category PER RUN
+# m <- c(100,200,500,1000) # m,n chosen from main manuscript
+# n <- c(3,5,7,9)
+# k <- seq(1,25,by=2)
+# These are used for continuous integration/testing:
+numruns <- 1  # Number of times to execute the simulations
+numsims <- 2 # Number of simulations in each category PER RUN
+m <- c(100, 1000) # m,n chosen from main manuscript
+n <- c(3,5)
+k <- seq(1,5,by=2)
+
 beta <- c(-2.5,-.15)
 S <- 2 # random intercept variance
 
@@ -107,8 +130,8 @@ for (i in 1:nrow(simstodoframe)) {
   idx <- idx+1
 }
 
-options(mc.cores = parallel::detectCores())
-RNGkind("L'Ecuyer-CMRG") # For reproducibility with parallel
+# options(mc.cores = parallel::detectCores())
+# RNGkind("L'Ecuyer-CMRG") # For reproducibility with parallel
 
 ### Function to execute simulation ###
 
@@ -201,48 +224,6 @@ dosim <- function(lst) {
        opt = list(aghqmm=opt_aghqmm,lme4=opt_lme4))
 }
 
-## TESTING: TO DELETE
-# lst <- simlist[[sample.int(length(simlist),1)]]
-# lst$m <- 1000
-# lst$n <- 5
-# lst$k <- 1
-# 
-# # try mucking about with the BFGS settings
-# lst$bfgsdelta<-1e-03
-# 
-# sim <- dosim(lst)
-# sim$aghqmm
-# sim$lme4
-# 
-# optfun <- function(tt)
-#   aghqmm::logmarglik(tt,y~x+(1|id),sim$data,k=1)$nll
-# gradfun <- function(tt)
-#   aghqmm::logmarglik(tt,y~x+(1|id),sim$data,k=1)$grad
-# 
-# tm <- Sys.time()
-# outsideopt <- optim(rep(0,3),optfun,gradfun,method="BFGS")
-# optimtime <- as.numeric(difftime(Sys.time(),tm,units='secs'))
-# cbind(outsideopt$par,sim$opt$lme4$theta)
-# c(optimtime,sim$lme4$simresults$totaltime)
-# 
-# 
-# c(sim$aghqmm$simresults$nll,sim$lme4$simresults$nll)
-# c(sim$aghqmm$simresults$normgrad,sim$lme4$simresults$normgrad)
-# c(sim$aghqmm$simresults$totaltime,sim$lme4$simresults$totaltime)
-# 
-# library(aghqmm)
-# formula <- y ~ x + (1|id)
-# data <- sim$data
-# k=lst$k
-# family=stats::binomial()
-# method = "both"
-# control = aghqmm_control()
-
-
-
-
-
-
 processsimulation <- function(sim) {
   
   out_aghqmm <- with(sim$paramvalues,data.frame(
@@ -290,7 +271,7 @@ processsimulation <- function(sim) {
 
 ### Do Simulations ###
 set.seed(92633)
-mc.reset.stream() # Reproducbility in parallel
+# mc.reset.stream() # Reproducbility in parallel
 # Do the simulations
 cat("Doing",length(simlist),"simulations...\n")
 tm <- Sys.time()
@@ -298,7 +279,8 @@ tm <- Sys.time()
 simruns <- list()
 length(simruns) <- numruns
 for (b in 1:numruns) {
-  simruns[[b]] <- mclapply(simlist,dosim)
+  # simruns[[b]] <- mclapply(simlist,dosim)
+  simruns[[b]] <- lapply(simlist,dosim)
 }
 sims <- Reduce(c,simruns)
 simtime <- as.numeric(difftime(Sys.time(),tm,units='secs'))
@@ -313,4 +295,267 @@ readr::write_csv(simsprocessed,file=file.path(resultspath,simsprocessedname))
 cat("Finished processing simulations.\n")
 cat("Wrote results to:",file.path(resultspath,simsprocessedname),"\n")
 
+simsummary <- simsprocessed
 
+m_labeller <- function(vl) paste0("m = ",vl)
+n_labeller <- function(vl) paste0("n = ",vl)
+
+## Barplot of successes ##
+successplot <- simsummary %>%
+  group_by(method,m,n,k) %>%
+  summarize(num=n(),successful=mean(successful)) %>%
+  mutate(se = sqrt(successful*(1-successful)/num),
+         lower = successful - 2*se,
+         upper = successful + 2*se
+  ) %>%
+  # ggplot(aes(x = factor(k),y = successful,fill = method)) +
+  ggplot(aes(x = factor(k),y = successful,group = method,colour = method)) +
+  theme_bw() +
+  facet_grid(n~m,labeller = labeller(m = m_labeller,n = n_labeller)) +
+  geom_line() +
+  geom_line(aes(y = lower),linetype='dashed') +
+  geom_line(aes(y = upper),linetype='dashed') +
+  # geom_bar(stat = "identity",position = position_dodge(),colour="black") +
+  # geom_errorbar(aes(ymin=lower,ymax=upper),position=position_dodge(width=.9),size=1,width=.5) +
+  # scale_fill_grey(start=.5,end=.8,labels = c("aghqmm" = "New Approach","lme4" = "lme4")) +
+  scale_colour_grey(start=.5,end=.8,labels = c("aghqmm" = "New Approach","lme4" = "lme4")) +
+  labs(
+    title = "Successful Simulations",
+    x = "Number of quadrature points",
+    y = "Proportion of simulations that were successful",
+    fill = "Method"
+  ) +
+  geom_hline(yintercept = 1,linetype='dotted') +
+  scale_x_discrete(breaks = seq(1,25,by=4)) +
+  scale_y_continuous(breaks = seq(0,1,by=.1),labels = scales::percent_format()) +
+  coord_cartesian(ylim=c(0,1))
+
+ggsave(filename = file.path(figurepath,"lme4-successplot.pdf"),plot = successplot,width=7,height=7)  
+
+## Boxplots of Absolute Computation Time ##
+# Show the absolute computation times; less relevant than relative, but necessary to know
+absolute_timeplot <- simsummary %>%
+  filter(successful == 1) %>%
+  ggplot(aes(x = factor(k),y = comptime,fill = method)) +
+  theme_bw() +
+  facet_grid(n~m,labeller = labeller(m = m_labeller,n = n_labeller)) +
+  geom_boxplot(outlier.size = .7,outlier.alpha = .05) +
+  scale_fill_grey(start=.5,end=.8,labels = c("aghqmm" = "New Approach","lme4" = "lme4")) +
+  labs(
+    title = "Absolute Computation Times (seconds)",
+    x = "Number of quadrature points",
+    y = "Computation time (seconds)",
+    fill = "Method"
+  ) +
+  scale_y_continuous(limits = c(0,2),breaks = seq(0,5,by=.25)) +
+  scale_x_discrete(breaks = seq(1,25,by=4)) +
+  coord_cartesian(ylim = c(0,2))
+
+ggsave(filename = file.path(figurepath,"lme4-absolutetimeplot.pdf"),plot = absolute_timeplot,width=7,height=7)  
+
+## Boxplots of Relative Computation Time ##
+# More relevant to direct comparison of a method; less affected by hardware
+
+aghq_times <- simsummary %>%
+  filter(method == "aghqmm") %>%
+  dplyr::select(idx,aghqtime = comptime)
+
+reltimes <- simsummary %>%
+  left_join(aghq_times,by='idx') %>%
+  mutate(reltime = comptime / aghqtime)
+
+
+relative_timeplot <- reltimes %>%
+  filter(successful == 1,method == "lme4") %>%
+  ggplot(aes(x = factor(k),y = reltime)) +
+  theme_bw() +
+  facet_grid(n~m,labeller = labeller(m = m_labeller,n = n_labeller)) +
+  geom_boxplot(outlier.size = .7,outlier.alpha = .05) +
+  labs(
+    title = "Relative Computation Times, lme4 / New Approach",
+    x = "Number of quadrature points",
+    y = "Relative computation time, lme4 / New Approach"
+  ) +
+  scale_y_continuous(breaks = seq(-1,10,by=1)) +
+  coord_cartesian(ylim = c(-1,10)) +
+  scale_x_discrete(breaks = seq(1,25,by=4)) +
+  geom_hline(yintercept=1,lty='dashed')
+
+ggsave(filename = file.path(figurepath,"lme4-relativetimeplot.pdf"),plot = relative_timeplot,width=7,height=7)  
+
+## Boxplots of log-likelihood values ##
+# Do the actual nll, by method
+
+raw_nll_plot <- simsummary %>%
+  filter(successful == 1) %>%
+  ggplot(aes(x = factor(k),y = nllavgbase10,fill = method)) +
+  theme_bw() +
+  facet_grid(n~m,labeller = labeller(m = m_labeller,n = n_labeller)) +
+  geom_boxplot(outlier.size = .7,outlier.alpha = .05) +
+  scale_fill_grey(start=.5,end=.8,labels = c("aghqmm" = "New Approach","lme4" = "lme4")) +
+  labs(
+    title = "Average negative log-likelihood values by method",
+    x = "Number of quadrature points",
+    y = bquote("-log"[10]~"likelihood / (mn)"),
+    fill = "Method"
+  ) +
+  scale_y_continuous(breaks = seq(.1,.2,by=.05)) +
+  scale_x_discrete(breaks = seq(1,25,by=4)) +
+  coord_cartesian(ylim = c(.1,.2))
+
+ggsave(filename = file.path(figurepath,"lme4-rawnllplot.pdf"),plot = raw_nll_plot,width=7,height=7)  
+
+## Boxplots of differences in log-likelihood values ##
+# Difference of nll for the same datasets by method
+
+aghq_nll <- simsummary %>%
+  filter(method == "aghqmm") %>%
+  dplyr::select(idx,aghqnll = nllavgbase10)
+
+nlldiffdata <- simsummary %>%
+  left_join(aghq_nll,by='idx') %>%
+  mutate(nlldiff = nllavgbase10 - aghqnll)
+
+nlldiffplot <- nlldiffdata %>%
+  filter(successful == 1,method == "lme4") %>%
+  ggplot(aes(x = factor(k),y = nlldiff)) +
+  theme_bw() +
+  facet_grid(n~m,labeller = labeller(m = m_labeller,n = n_labeller)) +
+  geom_boxplot(outlier.size = .7,outlier.alpha = .05) +
+  labs(
+    title = "Difference in negative log-likelihood, lme4 - New Approach",
+    x = "Number of quadrature points",
+    y = "Difference in negative log-likelihood, lme4 - New Approach"
+  ) +
+  # coord_cartesian(ylim = c(0,16)) +
+  geom_hline(yintercept=0,lty='dashed') +
+  scale_y_continuous(breaks = seq(-2,.2,by=.05)) +
+  scale_x_discrete(breaks = seq(1,25,by=4)) +
+  coord_cartesian(ylim = c(-.2,.2))
+
+
+ggsave(filename = file.path(figurepath,"lme4-nlldiffplot.pdf"),plot = nlldiffplot,width=7,height=7)  
+
+
+
+## Boxplots of gradient norm values ##
+# Actual values, by method
+
+raw_norm_plot <- simsummary %>%
+  filter(successful == 1) %>%
+  ggplot(aes(x = factor(k),y = normgradavg2base10log,fill = method)) +
+  theme_bw() +
+  facet_grid(n~m,labeller = labeller(m = m_labeller,n = n_labeller)) +
+  geom_boxplot(outlier.size = .7,outlier.alpha = .05) +
+  scale_fill_grey(start=.5,end=.8,labels = c("aghqmm" = "New Approach","lme4" = "lme4")) +
+  labs(
+    title = "Average log-norm of gradient by method",
+    x = "Number of quadrature points",
+    y = bquote("log"[10]~"||gradient||"[2]~"/ (mn)"),
+    fill = "Method"
+  ) +
+  scale_y_continuous(breaks = seq(-20,-2,by=2)) +
+  scale_x_discrete(breaks = seq(1,25,by=4)) +
+  coord_cartesian(ylim = c(-20,-2))
+
+ggsave(filename = file.path(figurepath,"lme4-rawnormplot.pdf"),plot = raw_norm_plot,width=7,height=7)  
+
+## Boxplots of differences in log-norm of gradient values ##
+# Difference of log gradient norms for the same datasets by method
+
+aghq_norms <- simsummary %>%
+  filter(method == "aghqmm") %>%
+  dplyr::select(idx,aghqnorm = normgradavg2base10log)
+
+normdiffdata <- simsummary %>%
+  left_join(aghq_norms,by='idx') %>%
+  mutate(normdiff = normgradavg2base10log - aghqnorm)
+
+normdiffplot <- normdiffdata %>%
+  filter(successful == 1,method == "lme4") %>%
+  ggplot(aes(x = factor(k),y = normdiff)) +
+  theme_bw() +
+  facet_grid(n~m,labeller = labeller(m = m_labeller,n = n_labeller)) +
+  geom_boxplot(outlier.size = .7,outlier.alpha = .05) +
+  labs(
+    title = "Difference in log-norm of gradient, lme4 - New Approach",
+    x = "Number of quadrature points",
+    y = "Difference in log-norm of gradient, lme4 - New Approach"
+  ) +
+  # coord_cartesian(ylim = c(0,16)) +
+  geom_hline(yintercept=0,lty='dashed') +
+  scale_y_continuous(breaks = seq(-8,12,by=2)) +
+  scale_x_discrete(breaks = seq(1,25,by=4)) +
+  coord_cartesian(ylim = c(-8,12))
+
+ggsave(filename = file.path(figurepath,"lme4-normdiffplot.pdf"),plot = normdiffplot,width=7,height=7)  
+
+### Main manuscript
+# Save the results to be shown in the main manuscript
+
+MAINTEXTSIZE <- 23
+
+if (1000 %in% m & 5 %in% n) {
+  main_reltimes <- reltimes %>%
+    filter(m==1000,n==5,successful == 1,method == "lme4")
+
+  main_nlldiffdata <- nlldiffdata %>%
+    filter(m==1000,n==5,successful == 1,method == "lme4")
+
+  main_normdiffdata <- normdiffdata %>%
+    filter(m==1000,n==5,successful == 1,method == "lme4")
+
+
+  relative_timeplot_manuscript <- main_reltimes %>%
+    ggplot(aes(x = factor(k),y = reltime)) +
+    theme_bw() +
+    geom_boxplot(outlier.size = .7,outlier.alpha = .05) +
+    labs(
+      title = "Rel. Comp. Times, lme4",
+      x = "Number of quadrature points",
+      y = "Comp. Time, lme4 / New Approach"
+    ) +
+    scale_y_continuous(breaks = seq(0,6,by=1)) +
+    coord_cartesian(ylim = c(0,6)) +
+    scale_x_discrete(breaks = seq(1,25,by=4)) +
+    geom_hline(yintercept=1,lty='dashed') +
+    theme(text = element_text(size=MAINTEXTSIZE))
+
+  ggsave(filename = file.path(figurepath,"lme4-relativetimeplot_manuscript.pdf"),plot = relative_timeplot_manuscript,width=7,height=7)
+
+
+  nlldiffplot_manuscript <- main_nlldiffdata %>%
+    ggplot(aes(x = factor(k),y = nlldiff)) +
+    theme_bw() +
+    geom_boxplot(outlier.size = .7,outlier.alpha = .05) +
+    labs(
+      title = "Diff. in neg. log-lik, lme4",
+      x = "Number of quadrature points",
+      y = "neg. log-lik, lme4 - New Approach"
+    ) +
+    geom_hline(yintercept=0,lty='dashed') +
+    scale_y_continuous(breaks = seq(-.1,.1,by=.02)) +
+    scale_x_discrete(breaks = seq(1,25,by=4)) +
+    coord_cartesian(ylim = c(-.1,.1)) +
+    theme(text = element_text(size=MAINTEXTSIZE))
+
+
+  ggsave(filename = file.path(figurepath,"lme4-nlldiffplot_manuscript.pdf"),plot = nlldiffplot_manuscript,width=7,height=7)
+
+  normdiffplot_manuscript <- main_normdiffdata %>%
+    ggplot(aes(x = factor(k),y = normdiff)) +
+    theme_bw() +
+    geom_boxplot(outlier.size = .7,outlier.alpha = .05) +
+    labs(
+      title = "Diff. in log-norm of grad, lme4",
+      x = "Number of quadrature points",
+      y = "log-norm(grad), lme4 - New Approach"
+    ) +
+    geom_hline(yintercept=0,lty='dashed') +
+    scale_y_continuous(breaks = seq(-4,12,by=2)) +
+    scale_x_discrete(breaks = seq(1,25,by=4)) +
+    coord_cartesian(ylim = c(-4,12)) +
+    theme(text = element_text(size=MAINTEXTSIZE))
+
+  ggsave(filename = file.path(figurepath,"lme4-normdiffplot_manuscript.pdf"),plot = normdiffplot_manuscript,width=7,height=7)
+}
